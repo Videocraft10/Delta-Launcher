c09f533f6f1d09ca206e4347d196b105f3445b7f
Videocraft <90282888+Videocraft10@users.noreply.github.com>
2024-12-09 10:36:26 -0500

    Update MainWindow.xaml.cs

--- Delta-Launcher/MainWindow.xaml.cs
+++ Delta-Launcher/MainWindow.xaml.cs
@@ -58,24 +58,13 @@ namespace Delta_Launcher
         {
             InitializeComponent();
             this.WindowState = WindowState.Minimized;
-            
-            
-            // Path to the executable file you want to run
-            //string filePath = @"C:\Users\franco.jourdan\Downloads\JetBrains.Rider-2024.3.exe"; // Change this to the path of your .exe file
 
-            //try
-            {
-                // Start the executable file
-                //Process.Start(filePath);
+            // Open the Startup window
+            Startup startupWindow = new Startup();
+            startupWindow.Show();
 
-                // Log success message to the console
-                //Console.WriteLine($"Successfully launched: {filePath}");
-            }
-            //catch (Exception ex)
-            {
-                // If there's an error, log the error message
-                //Console.WriteLine($"Error launching file: {ex.Message}");
-            }
+            Process.Start(@"C:\Users\franco.jourdan\Downloads\JetBrains.Rider-2024.3.exe");
         }
+        
     }
 }
